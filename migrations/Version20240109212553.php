5f6dea43004cff3e0e05af71d8ad88aa775cfb21
harolditoc <harold.chambilla@usil.pe>
2024-01-09 16:30:01 -0500

    Branch completo de Iteración 0

--- migrations/Version20240109212553.php
+++ migrations/Version20240109212553.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231231201856 extends AbstractMigration
+final class Version20240109212553 extends AbstractMigration
 {
     public function getDescription(): string
     {
