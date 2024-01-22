872848702cb4bb47d7fdc77680bbcf3d24325a01
Das-02 <josue.200297@hotmail.com>
2024-01-21 22:38:09 -0500

    Avance_vista_principal

--- assets/vue/controllers/App.vue
+++ assets/vue/controllers/App.vue
@@ -1,6 +1,20 @@
-<template>
-
+<template lang="">
+  <div>
+    <div class="text-bg-dark">Hello {{ name }}!</div>
+    <Tarjetas_VistaGeneral />
+  </div>
 </template>
-<script setup>
+<script>
 
+import Tarjetas_VistaGeneral from "./components/Tarjetas_VistaGeneral.vue";
+
+export default {
+  components: {
+    Tarjetas_VistaGeneral,
+  },
+  props: {
+    name: String,
+  },
+};
 </script>
+<style lang=""></style>
---
+++ assets/vue/controllers/components/Tarjetas_VistaGeneral.vue
@@ -0,0 +1,255 @@
+<template lang="">
+  <div class="container">
+    <div class="d-flex justify-content-start align-items-baseline mb-4">
+      <span class="fs-4 fw-semibold me-2">Productos</span>
+      <span><a href="#" class="fs-6 fw-semibold">Ver mas </a></span>
+    </div>
+
+    <div id="carouselExample" class="carousel slide">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div class="row row-cols-1 row-cols-md-4 g-4">
+            <div class="col">
+              <div class="card h-100 p-2">
+                <img src="../img/Cover.png" class="card-img-top" alt="..." />
+                <div class="card-body pt-4">
+                  <div class="row g-2">
+                    <div class="col-5">
+                      <span class="fs-5 fw-normal">S/. 255.00</span>
+                    </div>
+                    <div class="col-7 d-flex align-items-center">
+                      <span
+                        class="text-decoration-line-through fw-normal text-body-secondary"
+                        >S/. 250.00</span
+                      >
+                    </div>
+                    <div class="col-3">
+                      <span class="badge bg-dark text-wrap">Ahorras</span>
+                    </div>
+                    <div class="col-9">
+                      <span class="fs-6 fw-normal text-start">S/. 50.00</span>
+                    </div>
+                    <div class="col-12">
+                      <span
+                        >Shark IQ Robot Self-Empty XL Vacuum with Self-Empty
+                        Base, Home Mapping, RV1002AE, New...</span
+                      >
+                    </div>
+                    <div class="col-12">
+                      <span class="fa fa-star checked ms-1"></span>
+                      <span class="fa fa-star checked ms-1"></span>
+                      <span class="fa fa-star checked ms-1"></span>
+                      <span
+                        class="fa fa-star ms-1"
+                        style="color: #d9d9d9"
+                      ></span>
+                      <span
+                        class="fa fa-star ms-1"
+                        style="color: #d9d9d9"
+                      ></span>
+                    </div>
+                    <div class="col-12">
+                      <p><b>Envío gratis</b>, llega <b>en 3+ dias</b></p>
+                    </div>
+
+                    <div class="card-buttons">
+                      <button type>a</button>
+                      <button type>b</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card h-100">
+                <img src="../img/Cover.png" class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a short card.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card h-100">
+                <img src="../img/Portada.png" class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card h-100">
+                <img src="../img/Portada.png" class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="row row-cols-1 row-cols-md-4 g-4">
+            <div class="col">
+              <div class="card h-100">
+                <img src="../img/Cover.png" class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card h-100">
+                <img src="../img/Layout 1.png" class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a short card.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card h-100">
+                <img
+                  src="../img/Lomo Saltado.jpg"
+                  class="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card h-100">
+                <img src="../img/Portada.png" class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="row row-cols-1 row-cols-md-4 g-4">
+            <div class="col">
+              <div class="card h-100">
+                <img src="../img/Cover.png" class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card h-100">
+                <img src="../img/Layout 1.png" class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">This is a short card.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card h-100">
+                <img
+                  src="../img/Lomo Saltado.jpg"
+                  class="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card h-100">
+                <img src="../img/Portada.png" class="card-img-top" alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button
+        class="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExample"
+        data-bs-slide="prev"
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button
+        class="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExample"
+        data-bs-slide="next"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+.carousel-control-prev,
+.carousel-control-next {
+  width: 5%;
+}
+.checked {
+  color: #000000;
+}
+
+.card:hover {
+  background-color: rgb(23, 23, 43, 0.2);
+  z-index: 1;
+  transition: background-color 300ms ease-out;
+}
+.card:hover img,
+.card:hover span,
+.card:hover p {
+  opacity: 0.5; 
+}
+
+
+</style>
