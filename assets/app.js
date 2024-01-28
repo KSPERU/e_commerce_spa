922fd43013cdbe402defd21d6bfafede4b0f33b4
JEAN CARLOS TORRES ARMAS <jean.torresa@usil.pe>
2024-01-28 01:04:48 -0500

    Add Popup Carrito (Fixed Button)

--- assets/app.js
+++ assets/app.js
@@ -6,12 +6,17 @@ import './bootstrap';
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+
 import App from "./vue/controllers/App"; //Componente Principal en donde estarán todos los hijos.
 import App2 from "./vue/controllers/App2";
 import App3 from "./vue/controllers/App3";
-// import App2 from "./vue/controllers/App2"; //Ver componente de Listar productos
-// import App3 from "./vue/controllers/carrito"; //Ver componente de Listar productos
-// import app from "./vue/controllers/App";
+import PopupCarrito from "./vue/controllers/components/Btn_PopupCarrito";
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faCartShopping, faTrash, faAngleDown, faAngleUp} from '@fortawesome/free-solid-svg-icons';
+library.add(faCartShopping, faAngleDown, faAngleUp, faTrash);
+
 
 const pinia = createPinia();
 
@@ -19,6 +24,7 @@ const pinia = createPinia();
 const app = createApp(App);
 const app2 = createApp(App2); //Ver componentes de Listar productos
 const app3 = createApp(App3); 
+const popup_carrito = createApp(PopupCarrito); 
 
 app.use(pinia);
 app.mount('#app');
@@ -28,6 +34,11 @@ app2.mount('#app2');
 
 app3.use(pinia);
 app3.mount('#app3');
+
+popup_carrito.use(pinia);
+popup_carrito.component('font-awesome-icon', FontAwesomeIcon);
+popup_carrito.mount('#popup_carrito');
+
 
 const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
---
+++ assets/vue/controllers/components/Btn_PopupCarrito.vue
@@ -0,0 +1,70 @@
+<template>
+    <div>
+      <button @click="openPopup" class="popup_carrito position-fixed bottom-0 end-0 d-flex justify-content-center align-items-center cursor-pointer border-0 text-white">
+        <font-awesome-icon icon="cart-shopping" size="2x" class="popup-carrito-cart-icon"/>
+        <div class="popup_carrito_cantidad">
+          <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+          2
+          </span>
+        </div>
+      </button>
+      <div v-if="isOpen" class="popup-overlay">
+        <Content_PopupCarrito @closePopup="closePopup" />
+      </div>
+    </div>
+  </template>
+  
+
+
+  <script>
+  import Content_PopupCarrito from "./Content_PopupCarrito.vue";
+  
+  export default {
+    components: {
+      Content_PopupCarrito,
+    },
+    data() {
+      return {
+        isOpen: false,
+      };
+    },
+    methods: {
+      openPopup() {
+        this.isOpen = true;
+      },
+      closePopup() {
+        this.isOpen = false;
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .popup_carrito {
+    background-color: var(--color-azul-oscuro);
+    width: 90px;
+    height: 90px;
+    border-radius: 50%;
+    margin: 50px;
+  }
+  
+  .popup_carrito_cantidad{
+    height: 100%;
+    top: 12px;
+    left: 12px;
+    position: relative;
+  }
+
+  @media (max-width: 575.98px) {
+    .popup_carrito {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+    margin: 20px;
+  }
+  .popup-carrito-cart-icon{
+    font-size: 24px;
+  }
+  }
+  </style>
+  
