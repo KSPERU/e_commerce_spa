cbbb63721bfada86ff29a77f144a1825e8e7d57b
Das-02 <josue.200297@hotmail.com>
2024-01-30 23:19:49 -0500

    avance_index

--- assets/vue/controllers/components/Ofertas_Index.vue
+++ assets/vue/controllers/components/Ofertas_Index.vue
@@ -10,7 +10,7 @@
     <div id="carouselOfertas" class="carousel slide">
       <div class="carousel-inner d-flex">
         <div class="carousel-item active">
-          <div class="row row-cols-lg-4 row-cols-md-3 row-cols-sm-1 g-4">
+          <div class="row row-cols-lg-4 row-cols-md-2 row-cols-sm-2 g-4">
             <div class="col d-lg-block d-md-block d-sm-block">
               <div class="card h-100 p-2">
                 <img
@@ -20,147 +20,28 @@
                 />
                 <div class="card-body pt-4">
                   <div class="row g-2">
-                    <div class="col-lg-5 col-md-6 col-4">
-                      <span class="size-18 fw-normal">S/. 255.00</span>
+                    <div class="col-lg-5 col-md-5 col-sm-12">
+                      <span class="size-18 fw-normal">S/. 66 255.00</span>
                     </div>
-                    <div class="col-lg-7 col-md-6 col-8 d-flex align-items-end">
+                    <div
+                      class="col-lg-7 col-md-7 col-sm-12 d-flex align-items-end d-lg-inline d-md-inline d-none"
+                    >
                       <span
                         class="size-15 text-decoration-line-through fw-normal text-body-secondary"
                         >S/. 250.00</span
                       >
                     </div>
-                    <div class="col-lg-4 col-md-6 col-3">
-                      <span class="size-14 badge bg-dark text-wrap"
-                        >Ahorras</span
-                      >
+                    <div
+                      class="col-lg-4 col-md-4 col-sm-6"
+                      :class="{ VistaMovil: mostrarOferta }"
+                    >
+                      <span class="size-14 badge bg-dark text-wrap">{{
+                        mostrarOferta ? "Oferta" : "Ahorras"
+                      }}</span>
                     </div>
-                    <div class="col-lg-8 col-md-6 col-9">
-                      <span class="size-14 fw-normal text-start"
-                        >S/. 50.00</span
-                      >
-                    </div>
-                    <div class="col-lg-12 col-md-12 col-12">
-                      <p class="size-14 custom-truncate m-0">
-                        Shark IQ Robot Self-Empty XL Vacuum with Self-Empty
-                        Base, Home Mapping, RV1002AE, New Shark IQ Robot
-                        Self-Empty XL Vacuum with Self-Empty Base, Home Mapping,
-                        RV1002AE, New
-                      </p>
-                    </div>
-                    <div class="col-lg-12 col-md-12 col-sm-12">
-                      <span class="fa fa-star checked ms-1"></span>
-                      <span class="fa fa-star checked ms-1"></span>
-                      <span class="fa fa-star checked ms-1"></span>
-                      <span
-                        class="fa fa-star ms-1"
-                        style="color: #d9d9d9"
-                      ></span>
-                      <span
-                        class="fa fa-star ms-1"
-                        style="color: #d9d9d9"
-                      ></span>
-                      
-                      <span class="ms-2 text-body-secondary">
-                        20
-                      </span>
-                    </div>
-                    <div class="col-lg-12 col-md-12 col-sm-12">
-                      <p class="size-14">
-                        <span class="fw-bold">Envío gratis</span>, llega
-                        <span class="fw-bold">en 3+ dias</span>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col d-lg-block d-md-block d-none">
-              <div class="card h-100 p-2">
-                <img
-                  src="../img/aspiradora.png"
-                  class="card-img-top"
-                  alt="..."
-                />
-                <div class="card-body pt-4">
-                  <div class="row g-2">
-                    <div class="col-lg-5 col-md-6 col-4">
-                      <span class="size-18 fw-normal">S/. 255.00</span>
-                    </div>
-                    <div class="col-lg-7 col-md-6 col-8 d-flex align-items-end">
-                      <span
-                        class="size-15 text-decoration-line-through fw-normal text-body-secondary"
-                        >S/. 250.00</span
-                      >
-                    </div>
-                    <div class="col-lg-4 col-md-6 col-3">
-                      <span class="size-14 badge bg-dark text-wrap"
-                        >Ahorras</span
-                      >
-                    </div>
-                    <div class="col-lg-8 col-md-6 col-9">
-                      <span class="size-14 fw-normal text-start"
-                        >S/. 50.00</span
-                      >
-                    </div>
-                    <div class="col-lg-12 col-md-12 col-12">
-                      <p class="size-14 custom-truncate m-0">
-                        Shark IQ Robot Self-Empty XL Vacuum with Self-Empty
-                        Base, Home Mapping, RV1002AE, New Shark IQ Robot
-                        Self-Empty XL Vacuum with Self-Empty Base, Home Mapping,
-                        RV1002AE, New
-                      </p>
-                    </div>
-                    <div class="col-lg-12 col-md-12 col-sm-12">
-                      <span class="fa fa-star checked ms-1"></span>
-                      <span class="fa fa-star checked ms-1"></span>
-                      <span class="fa fa-star checked ms-1"></span>
-                      <span
-                        class="fa fa-star ms-1"
-                        style="color: #d9d9d9"
-                      ></span>
-                      <span
-                        class="fa fa-star ms-1"
-                        style="color: #d9d9d9"
-                      ></span>
-              
-                      <span class="ms-2 text-body-secondary">
-                        20
-                      </span>
-                    </div>
-                    <div class="col-lg-12 col-md-12 col-sm-12">
-                      <p class="size-14">
-                        <span class="fw-bold">Envío gratis</span>, llega
-                        <span class="fw-bold">en 3+ dias</span>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col d-lg-block d-md-block d-none">
-              <div class="card h-100 p-2">
-                <img
-                  src="../img/aspiradora.png"
-                  class="card-img-top"
-                  alt="..."
-                />
-                <div class="card-body pt-4">
-                  <div class="row g-2">
-                    <div class="col-lg-5 col-md-6 col-4">
-                      <span class="size-18 fw-normal">S/. 255.00</span>
-                    </div>
-                    <div class="col-lg-7 col-md-6 col-8 d-flex align-items-end">
-                      <span
-                        class="size-15 text-decoration-line-through fw-normal text-body-secondary"
-                        >S/. 250.00</span
-                      >
-                    </div>
-                    <div class="col-lg-4 col-md-6 col-3">
-                      <span class="size-14 badge bg-dark text-wrap"
-                        >Ahorras</span
-                      >
-                    </div>
-                    <div class="col-lg-8 col-md-6 col-9">
+                    <div
+                      class="col-lg-8 col-md-8 col-sm-6 d-none d-lg-inline d-md-inline"
+                    >
                       <span class="size-14 fw-normal text-start"
                         >S/. 50.00</span
                       >
@@ -186,8 +67,9 @@
                         style="color: #d9d9d9"
                       ></span>
 
-                      
-                      <span class="ms-2 text-body-secondary">
+                      <span
+                        class="ms-2 text-body-secondary d-lg-inline d-md-inline d-none"
+                      >
                         20
                       </span>
                     </div>
@@ -201,7 +83,8 @@
                 </div>
               </div>
             </div>
-            <div class="col d-lg-block d-none">
+
+            <div class="col d-lg-block d-md-block d-sm-block">
               <div class="card h-100 p-2">
                 <img
                   src="../img/aspiradora.png"
@@ -210,21 +93,28 @@
                 />
                 <div class="card-body pt-4">
                   <div class="row g-2">
-                    <div class="col-lg-5 col-md-6 col-4">
-                      <span class="size-18 fw-normal">S/. 255.00</span>
+                    <div class="col-lg-5 col-md-5 col-sm-12">
+                      <span class="size-18 fw-normal">S/. 66 255.00</span>
                     </div>
-                    <div class="col-lg-7 col-md-6 col-8 d-flex align-items-end">
+                    <div
+                      class="col-lg-7 col-md-7 col-sm-12 d-flex align-items-end d-lg-inline d-md-inline d-none"
+                    >
                       <span
                         class="size-15 text-decoration-line-through fw-normal text-body-secondary"
                         >S/. 250.00</span
                       >
                     </div>
-                    <div class="col-lg-4 col-md-6 col-3">
-                      <span class="size-14 badge bg-dark text-wrap"
-                        >Ahorras</span
-                      >
+                    <div
+                      class="col-lg-4 col-md-4 col-sm-6"
+                      :class="{ VistaMovil: mostrarOferta }"
+                    >
+                      <span class="size-14 badge bg-dark text-wrap">{{
+                        mostrarOferta ? "Oferta" : "Ahorras"
+                      }}</span>
                     </div>
-                    <div class="col-lg-8 col-md-6 col-9">
+                    <div
+                      class="col-lg-8 col-md-8 col-sm-6 d-none d-lg-inline d-md-inline"
+                    >
                       <span class="size-14 fw-normal text-start"
                         >S/. 50.00</span
                       >
@@ -250,8 +140,9 @@
                         style="color: #d9d9d9"
                       ></span>
 
-                      
-                      <span class="ms-2 text-body-secondary">
+                      <span
+                        class="ms-2 text-body-secondary d-lg-inline d-md-inline d-none"
+                      >
                         20
                       </span>
                     </div>
@@ -265,6 +156,154 @@
                 </div>
               </div>
             </div>
+
+            <div class="col d-lg-block d-md-block d-sm-block">
+              <div class="card h-100 p-2">
+                <img
+                  src="../img/aspiradora.png"
+                  class="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body pt-4">
+                  <div class="row g-2">
+                    <div class="col-lg-5 col-md-5 col-sm-12">
+                      <span class="size-18 fw-normal">S/. 66 255.00</span>
+                    </div>
+                    <div
+                      class="col-lg-7 col-md-7 col-sm-12 d-flex align-items-end d-lg-inline d-md-inline d-none"
+                    >
+                      <span
+                        class="size-15 text-decoration-line-through fw-normal text-body-secondary"
+                        >S/. 250.00</span
+                      >
+                    </div>
+                    <div
+                      class="col-lg-4 col-md-4 col-sm-6"
+                      :class="{ VistaMovil: mostrarOferta }"
+                    >
+                      <span class="size-14 badge bg-dark text-wrap">{{
+                        mostrarOferta ? "Oferta" : "Ahorras"
+                      }}</span>
+                    </div>
+                    <div
+                      class="col-lg-8 col-md-8 col-sm-6 d-none d-lg-inline d-md-inline"
+                    >
+                      <span class="size-14 fw-normal text-start"
+                        >S/. 50.00</span
+                      >
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-12">
+                      <p class="size-14 custom-truncate m-0">
+                        Shark IQ Robot Self-Empty XL Vacuum with Self-Empty
+                        Base, Home Mapping, RV1002AE, New Shark IQ Robot
+                        Self-Empty XL Vacuum with Self-Empty Base, Home Mapping,
+                        RV1002AE, New
+                      </p>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                      <span class="fa fa-star checked ms-1"></span>
+                      <span class="fa fa-star checked ms-1"></span>
+                      <span class="fa fa-star checked ms-1"></span>
+                      <span
+                        class="fa fa-star ms-1"
+                        style="color: #d9d9d9"
+                      ></span>
+                      <span
+                        class="fa fa-star ms-1"
+                        style="color: #d9d9d9"
+                      ></span>
+
+                      <span
+                        class="ms-2 text-body-secondary d-lg-inline d-md-inline d-none"
+                      >
+                        20
+                      </span>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                      <p class="size-14">
+                        <span class="fw-bold">Envío gratis</span>, llega
+                        <span class="fw-bold">en 3+ dias</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="col d-lg-block d-md-block d-sm-block">
+              <div class="card h-100 p-2">
+                <img
+                  src="../img/aspiradora.png"
+                  class="card-img-top"
+                  alt="..."
+                />
+                <div class="card-body pt-4">
+                  <div class="row g-2">
+                    <div class="col-lg-5 col-md-5 col-sm-12">
+                      <span class="size-18 fw-normal">S/. 66 255.00</span>
+                    </div>
+                    <div
+                      class="col-lg-7 col-md-7 col-sm-12 d-flex align-items-end d-lg-inline d-md-inline d-none"
+                    >
+                      <span
+                        class="size-15 text-decoration-line-through fw-normal text-body-secondary"
+                        >S/. 250.00</span
+                      >
+                    </div>
+                    <div
+                      class="col-lg-4 col-md-4 col-sm-6"
+                      :class="{ VistaMovil: mostrarOferta }"
+                    >
+                      <span class="size-14 badge bg-dark text-wrap">{{
+                        mostrarOferta ? "Oferta" : "Ahorras"
+                      }}</span>
+                    </div>
+                    <div
+                      class="col-lg-8 col-md-8 col-sm-6 d-none d-lg-inline d-md-inline"
+                    >
+                      <span class="size-14 fw-normal text-start"
+                        >S/. 50.00</span
+                      >
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-12">
+                      <p class="size-14 custom-truncate m-0">
+                        Shark IQ Robot Self-Empty XL Vacuum with Self-Empty
+                        Base, Home Mapping, RV1002AE, New Shark IQ Robot
+                        Self-Empty XL Vacuum with Self-Empty Base, Home Mapping,
+                        RV1002AE, New
+                      </p>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                      <span class="fa fa-star checked ms-1"></span>
+                      <span class="fa fa-star checked ms-1"></span>
+                      <span class="fa fa-star checked ms-1"></span>
+                      <span
+                        class="fa fa-star ms-1"
+                        style="color: #d9d9d9"
+                      ></span>
+                      <span
+                        class="fa fa-star ms-1"
+                        style="color: #d9d9d9"
+                      ></span>
+
+                      <span
+                        class="ms-2 text-body-secondary d-lg-inline d-md-inline d-none"
+                      >
+                        20
+                      </span>
+                    </div>
+                    <div class="col-lg-12 col-md-12 col-sm-12">
+                      <p class="size-14">
+                        <span class="fw-bold">Envío gratis</span>, llega
+                        <span class="fw-bold">en 3+ dias</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
           </div>
         </div>
         <div class="carousel-item">
@@ -314,9 +353,7 @@
                         style="color: #d9d9d9"
                       ></span>
 
-                      <span class="ms-2 text-body-secondary">
-                        20
-                      </span>
+                      <span class="ms-2 text-body-secondary"> 20 </span>
                     </div>
                     <div class="col-lg-12 col-md-12 col-sm-12">
                       <p class="size-14">
@@ -354,7 +391,24 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      mostrarOferta: window.innerWidth < 426.8,
+    };
+  },
+  mounted() {
+    window.addEventListener("resize", this.actualizarEstado);
+  },
+  methods: {
+    actualizarEstado() {
+      this.mostrarOferta = window.innerWidth < 426.8;
+    },
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.actualizarEstado);
+  },
+};
 </script>
 <style scoped>
 .carousel-control-prev,
@@ -385,5 +439,18 @@ img {
   -webkit-line-clamp: 4; /* Número de líneas que deseas mostrar */
   -webkit-box-orient: vertical;
   overflow: hidden;
+}
+
+@media (max-width: 426.8px) {
+  img {
+    aspect-ratio: 7 / 6;
+  }
+  .custom-truncate {
+    -webkit-line-clamp: 2;
+  }
+}
+
+.VistaMovil span.text-wrap {
+  display: inline;
 }
 </style>
