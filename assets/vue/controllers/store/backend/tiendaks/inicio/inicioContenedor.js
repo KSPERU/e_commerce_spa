982b78db1da3ce4950b7627d6d70f8ecda7db527
M4HR <huertasramosmanuelangel@gmail.com>
2024-02-26 17:13:16 -0500

    100% vista inicio

--- assets/vue/controllers/store/backend/tiendaks/inicio/inicioContenedor.js
+++ assets/vue/controllers/store/backend/tiendaks/inicio/inicioContenedor.js
@@ -4,11 +4,13 @@ import { defineStore } from 'pinia';
 export const useInicioContenedor = defineStore('inicioContenedor', {
     state: () => ({
       productos: [],
-      productos_ofertados: []
+      productos_ofertados: [],
+      categorias_populares: []
     }),
     getters: {
       PRODUCTOS(state) {return state.productos},
-      PRODUCTOSOFERTADOS(state) {return state.productos_ofertados}
+      PRODUCTOSOFERTADOS(state) {return state.productos_ofertados},
+      CATEGORIASPOPULARES(state) {return state.categorias_populares}
     },
     actions: {
       async getVerProductoListado(datos) {
@@ -25,6 +27,14 @@ export const useInicioContenedor = defineStore('inicioContenedor', {
             this.productos_ofertados = response.data;
         } catch (error) {
             console.log("Un error" + error.response.data)
+        }
+      },
+      async getVerCategoriaListadoPopulares(datos) {
+        try {
+            const response = await axios.post('/api/producto/listar/producto/listadocategorias', datos);  
+            this.categorias_populares = response.data;
+        } catch (error) {
+            console.log("Un error " + error.response.data)
         }
       },
     },
